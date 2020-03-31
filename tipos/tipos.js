b47ff9679be642d7d458b163c0fe9ef9ce164faf
junior duarte <jr.duartee.93@gmail.com>
2020-03-31 18:27:56 -0300

    Módulo de tipos finish

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -16,3 +16,60 @@ var Cor;
 })(Cor || (Cor = {}));
 var minhaCor = Cor.Verde;
 console.log(minhaCor);
+//funções
+// tipo void não retorna nada
+function retornaMeuNome() {
+    return nome;
+}
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+console.log(multiplicar(20, 20));
+//tipo função
+var calculo;
+calculo = multiplicar;
+//objetos
+var usuarios = {
+    nome: "Junior",
+    idade: 27
+};
+//Desafio
+var funcionario = {
+    supervisores: ["Junior", "Joice", "Duarte"],
+    batePonto: function (horas) {
+        if (horas <= 8) {
+            return "Ponto Normal";
+        }
+        else {
+            return "Fora do horário";
+        }
+    }
+};
+var funcionario2 = {
+    supervisores: ["Junior", "Joice", "Duarte"],
+    batePonto: function (horas) {
+        if (horas <= 8) {
+            return "Ponto Normal";
+        }
+        else {
+            return "Fora do horário";
+        }
+    }
+};
+//Union types - dois tipos
+var nota = 10;
+//Tipo never - erro,laço infinito...
+function falha(msg) {
+    throw new Error(msg);
+}
+var contaBancaria = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+var correntista = {
+    nome: "Junior",
+    contaBancaria: contaBancaria,
+    contatos: ["97016-3734", "2235-6908"]
+};
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -10,7 +10,6 @@ let hobbies: any[] = ["Games", "Futebol", 1];
 let endereco: [string, number] = ["Av. principal", 22];
 
 //enums
-
 enum Cor {
   Cinza = "Cinza",
   Verde = "Verde",
@@ -19,3 +18,90 @@ enum Cor {
 
 let minhaCor: Cor = Cor.Verde;
 console.log(minhaCor);
+
+//funções
+// tipo void não retorna nada
+function retornaMeuNome(): string {
+  return nome;
+}
+
+function multiplicar(numA: number, numB: number): number {
+  return numA * numB;
+}
+
+console.log(multiplicar(20, 20));
+
+//tipo função
+let calculo: (numA: number, numB: number) => number;
+calculo = multiplicar;
+
+//objetos
+let usuarios: { nome: string; idade: number } = {
+  nome: "Junior",
+  idade: 27
+};
+
+//Desafio
+let funcionario: {
+  supervisores: string[];
+  batePonto: (horas: number) => string;
+} = {
+  supervisores: ["Junior", "Joice", "Duarte"],
+  batePonto(horas: number): string {
+    if (horas <= 8) {
+      return "Ponto Normal";
+    } else {
+      return "Fora do horário";
+    }
+  }
+};
+
+//Alias
+type Funcionario = {
+  supervisores: string[];
+  batePonto: (horas: number) => string;
+};
+
+let funcionario2: Funcionario = {
+  supervisores: ["Junior", "Joice", "Duarte"],
+  batePonto(horas: number): string {
+    if (horas <= 8) {
+      return "Ponto Normal";
+    } else {
+      return "Fora do horário";
+    }
+  }
+};
+
+//Union types - dois tipos
+let nota: string | number = 10;
+
+//Tipo never - erro,laço infinito...
+function falha(msg: string) {
+  throw new Error(msg);
+}
+
+//Desafio 2
+type ContaBancaria = {
+  saldo: number;
+  depositar: (valor: number) => void;
+};
+
+let contaBancaria: ContaBancaria = {
+  saldo: 3456,
+  depositar(valor: number) {
+    this.saldo += valor;
+  }
+};
+
+type Correntista = {
+  nome: string;
+  contaBancaria: ContaBancaria;
+  contatos: string[];
+};
+
+let correntista: Correntista = {
+  nome: "Junior",
+  contaBancaria: contaBancaria,
+  contatos: ["97016-3734", "2235-6908"]
+};
